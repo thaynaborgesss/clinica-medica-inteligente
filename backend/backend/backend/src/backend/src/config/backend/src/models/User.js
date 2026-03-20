6c88fef544738790435258951683710a95253370
Thayná <thaynaborgess@yahoo.com.br>
2026-03-20 20:26:29 -0300

    Create User.js

---
+++ backend/backend/backend/src/backend/src/config/backend/src/models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true
+    },
+    password: {
+      type: String,
+      required: true
+    },
+    role: {
+      type: String,
+      enum: ['paciente', 'secretario'],
+      default: 'paciente'
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+module.exports = mongoose.model('User', userSchema);
